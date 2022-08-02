53a3a96afd3dc2c57904aa4f02bc117a676b7e12
Amanda Sakuma, Danny Lucas <lannyducas@gmail.com>
2022-08-02 11:23:21 -0400

    interpolated variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let i = 1;
 const url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4pCbIkzrX75qDLPyhvCvGL4ijmyhsWMm'
 
 fetch(url)
@@ -5,7 +6,8 @@ fetch(url)
     // .then(renderNewsFeed(data))
     .then((data) => { 
         data.results.forEach((article) => {
-            renderNewsFeed(article)
+            renderNewsFeed(article);
+            i++;
         })
     })
 
@@ -15,12 +17,12 @@ function renderNewsFeed(article) {
 
     //get the newsfeed elements
     ////story card div, headline, img
-    const storyCard1 = document.querySelector('#feed-story-card-1')
-    const headline1 = document.querySelector('#feed-headline-1')
-    const img1 = document.querySelector('#feed-img-1')
+    const storyCard1 = document.querySelector(`#feed-story-card-${i}`)
+    const headline1 = document.querySelector(`#feed-headline-${i}`)
+    const img1 = document.querySelector(`#feed-img-${i}`)
 
     //[populate the story card with data for headline and image]
-    headline1.innerText = article.section
+    headline1.innerText = article.title;
     console.log(headline1)
 
 }
