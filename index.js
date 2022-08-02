4797768a5856e07e7d85d77a9fdd1871946e61c7
Amanda Sakuma <amandasakuma@gmail.com>
2022-08-02 10:44:48 -0400

    fetch statement

---
+++ index.js
@@ -0,0 +1,26 @@
+const url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4pCbIkzrX75qDLPyhvCvGL4ijmyhsWMm'
+
+fetch(url)
+    .then(res => res.json())
+    // .then(renderNewsFeed(data))
+    .then((data) => {
+        data.forEach((article) => {
+            renderNewsFeed(article)
+        })
+    })
+
+function renderNewsFeed(article) {
+    // getthe newsfeed container
+    const newsFeedContainer = document.querySelector('#article-feed')
+
+    //get the newsfeed elements
+    ////story card div, headline, img
+    const storyCard1 = document.querySelector('#feed-story-card-1')
+    const headline1 = document.querySelector('#feed-headline-1')
+    const img1 = document.querySelector('#feed-img-1')
+
+    //[populate the story card with data for headline and image]
+    headline1.innerText = article.section
+    console.log(headline1)
+
+}
